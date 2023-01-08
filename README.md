2fb857d60790af3d66367485cfa64d6591fd0038
Jose Daniel Velasquez <jvelasquez@thefactoryhka.com>
2023-01-08 17:13:28 -0500

    Path Request y Response

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ pip install uvicorn
 
 #Levantar API
 uvicorn main:app --reload
+
+
+#home
+http://localhost:8000/
+
+#swagger
+http://localhost:8000/docs#/
+
+#redoc
+http://localhost:8000/docs#/
--- fast-api-hello-world/main.py
+++ fast-api-hello-world/main.py
@@ -1,7 +1,40 @@
-from fastapi import FastAPI
+#Python
+from typing import Optional,List
 
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
+from fastapi import FastAPI
+from fastapi import Body
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"Hello": "World"}
+#Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
+
+@app.get("/") #path operation decorator
+def home(): #path operation function
+    return {"Hello": "World"} #JSON
+
+#path parameters
+@app.get("/tweets/{id}")
+def tweet(id):
+    return {"params": id}
+
+#Query Parameters
+#Parameters Separator &
+#/users/{user_id}/details?age=20&height=177
+@app.get("/users/{user_id}/details")
+def usersId(users_id):
+    return {"id": users_id}
+
+#Request Body y Response Body
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
