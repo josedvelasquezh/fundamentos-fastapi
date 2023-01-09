954645ccae86ecbc459cc81a2e607066453d2b99
Jose Daniel Velasquez <jvelasquez@thefactoryhka.com>
2023-01-08 22:10:04 -0500

    Validaciones Tipos de datos

--- fast-api-hello-world/main.py
+++ fast-api-hello-world/main.py
@@ -1,8 +1,10 @@
 #Python
-from typing import Optional,List
+from typing import Optional
+from enum import Enum
 
 #Pydantic
 from pydantic import BaseModel
+from pydantic import Field
 
 #FastAPI
 from fastapi import FastAPI
@@ -11,12 +13,19 @@ from fastapi import Query, Path
 app = FastAPI()
 
 #Models
+class HairColor(Enum):
+    white = "white"
+    browm = "browm"
+    black = "black"
+    blonde = "blonde"
+    red = "red"
+
 class Person(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    hair_color: Optional[str] = None
-    is_married: Optional[bool] = None
+    first_name: str = Field(...,min_length=1, max_length=50)
+    last_name: str = Field(...,min_length=1, max_length=50)
+    age: int = Field(..., gt=0, le=115)
+    hair_color: Optional[HairColor] = Field(default=None)
+    is_married: Optional[bool] = Field(default=None)
 
 class Location(BaseModel):
     city: str
@@ -95,3 +104,17 @@ def update_person(
     results.update(location.dict())
     return results
 
+
+
+#Tipos Especiales
+#Enum
+#HttpUrl
+#FilePath
+#DirectoryPath
+#EmailStr
+#PaymentCardNumber
+#IpvAnyAddress
+#NegativeFloat
+#PositiveFloat
+#NegativeInt
+#PositiveInt
